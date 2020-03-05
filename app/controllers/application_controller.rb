2db4e945c8b1e91b768a836d91d8b29002b58275
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-05 22:27:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    binding.pry
     if !params[:username] == "" && !params[:passoword] == ""
-      binding.pry
       user = User.new(:username => params[:username], :password => params[:password])
       user.save
       redirect '/login'
