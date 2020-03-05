07a6f9cc55d00d85cacf76c262f2059d7132cadb
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-05 22:19:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if params[:username] == "" || params[:passowrd] == ""
+    if !params[:username] == "" && !params[:passowrd] == ""
       user = User.new(:username => params[:username], :password => params[:password])
 
   	  if user.save
